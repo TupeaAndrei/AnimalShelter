916989b5b4200bec9a45c4f21624495c560ff3a0
laviniadurac <lvi.lavi@yahoo.com>
2021-10-26 20:11:57 +0300

    Added DbContext

---
+++ AnimalShelter/AnimalShelter.DAL/Migrations/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using AnimalShelter.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelter.DAL.Migrations
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AdoptionPaper> AdoptionPapers { get; set; }
+        public DbSet<Animal> Animals { get; set; }
+        public DbSet<Cage> Cages { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeType> EmployeeTypes { get; set; }
+        public DbSet<MedicalCare> Interventions { get; set; }
+        public DbSet<MedicalJournal> MedicalJournals { get; set; }
+        public DbSet<Visiter> Visiters { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        }
+}
