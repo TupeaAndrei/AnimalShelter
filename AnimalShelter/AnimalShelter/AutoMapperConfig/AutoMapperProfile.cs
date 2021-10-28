958c496b4587994543c9d4366d1799101857b482
TupeaAndrei <59830697+TupeaAndrei@users.noreply.github.com>
2021-10-28 16:37:19 +0300

    Added and configured AutoMapper extension.
    Configured automapper extension to work from each entity to it's DTO and reverse.

---
+++ AnimalShelter/AnimalShelter/AutoMapperConfig/AutoMapperProfile.cs
@@ -0,0 +1,25 @@
+﻿using AnimalShelter.DAL.Entities;
+using AnimalShelter.TL.DTO;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalShelter.AutoMapperConfig
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<AdoptionPaper, AdoptionPaperDTO>().ReverseMap();
+            CreateMap<Animal, AnimalDTO>().ReverseMap();
+            CreateMap<Cage, CageDTO>().ReverseMap();
+            CreateMap<Employee, EmployeeDTO>().ReverseMap();
+            CreateMap<EmployeeType, EmployeeTypeDTO>().ReverseMap();
+            CreateMap<MedicalCare, MedicalCareDTO>().ReverseMap();
+            CreateMap<MedicalJournal, MedicalJournalDTO>().ReverseMap();
+            CreateMap<Visitor, VisitorDTO>().ReverseMap();
+        }
+    }
+}
--- AnimalShelter/AnimalShelter/Startup.cs
+++ AnimalShelter/AnimalShelter/Startup.cs
@@ -25,7 +25,7 @@ namespace AnimalShelter
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddAutoMapper(typeof(Startup));
             var connectionString =
                 "Server=(localdb)\\MSSQLLocalDB;Database=AnimalShelterD;Trusted_Connection=True;MultipleActiveResultSets=true";
 
